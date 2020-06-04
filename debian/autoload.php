08d265383b70c8cd085c80724d2f2f414b743205
Dave Pifke <dpifke@wikimedia.org>
2020-06-03 19:29:34 -0700

    Package XHGui
    Bug: T254310
    Change-Id: I581a5861090bb3f4cac1113056021261fd6af2e0

---
+++ debian/autoload.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file gets installed as /usr/share/xhgui/vendor/autoload.php.
+ *
+ * It sources the autoload.php files built by phpab during Debian packaging.
+ */
+
+require_once '/usr/share/php/Twig/autoload.php';
+require_once '/usr/share/php/Pimple/autoload.php';
+require_once '/usr/share/php/Slim/autoload.php';
+require_once '/usr/share/php/Slim/Views/autoload.php';
+require_once '/usr/share/php/Xhgui/collector-autoload.php';
+require_once '/usr/share/xhgui/src/autoload.php';
